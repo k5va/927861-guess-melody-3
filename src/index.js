00de9e6ef1cbd006464bf84fa37c3fe54653ca73
k5va <k5va@yandex.ru>
2020-02-05 10:39:28 +0300

    Passed Settings.MAX_ERRORS_COUNT to App

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 
+const Settings = {
+  MAX_ERRORS_COUNT: 3
+};
+
 ReactDOM.render(
-    <App />,
+    <App maxErrorsCount={Settings.MAX_ERRORS_COUNT} />,
     document.querySelector(`#root`)
 );
