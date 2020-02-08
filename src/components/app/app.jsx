d6ec1d52117b377c375b7d03f376cfc87e335c1a
k5va <k5va@yandex.ru>
2020-02-08 17:13:28 +0300

    Added prop types to App comp.

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import PropTypes from "prop-types";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
 
-// eslint-disable-next-line react/prop-types
 const App = ({maxErrorsCount}) => {
   return (
     <WelcomeScreen maxErrorsCount={maxErrorsCount} />
   );
+};
+
+App.propTypes = {
+  maxErrorsCount: PropTypes.number.isRequired
 };
 
 export default App;
