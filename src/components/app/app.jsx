ad17589eb71cd5f5e3ecb1707f835ce7d8eb669b
k5va <k5va@yandex.ru>
2020-02-12 21:18:19 +0300

    Added router to App

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,25 @@
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import WelcomeScreen from "../welcome-screen/welcome-screen";
+import ArtistQuestionScreen from "../artist-question-screen/artist-question-screen";
+import GenreQuestionScreen from "../genre-question-screen/genre-question-screen";
 
 const App = ({maxErrorsCount}) => {
   const handleWelcomeButtonClick = () => {};
 
   return (
-    <WelcomeScreen maxErrorsCount={maxErrorsCount} onWelcomeButtonClick={handleWelcomeButtonClick} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <WelcomeScreen maxErrorsCount={maxErrorsCount} onWelcomeButtonClick={handleWelcomeButtonClick} />
+        </Route>
+        <Route exact path="/dev-artist">
+          <ArtistQuestionScreen />
+        </Route>
+        <Route exact path="/dev-genre">
+          <GenreQuestionScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
