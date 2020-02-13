3c81f82461619d0c6221887dedd5aa3c39ddde25
k5va <k5va@yandex.ru>
2020-02-13 09:43:46 +0300

    Updated App snapshot test

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -2,7 +2,45 @@ import App from "./app";
 
 const MAX_ERRORS_COUNT = 3;
 
+const questions = [
+  {
+    type: `genre`,
+    genre: `rock`,
+    answers: [{
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      genre: `rock`,
+    }, {
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      genre: `blues`,
+    }, {
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      genre: `jazz`,
+    }, {
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      genre: `rock`,
+    }],
+  },
+  {
+    type: `artist`,
+    song: {
+      artist: `Jim Beam`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+    },
+    answers: [{
+      picture: `https://api.adorable.io/avatars/128/1`,
+      artist: `John Snow`,
+    }, {
+      picture: `https://api.adorable.io/avatars/128/2`,
+      artist: `Jack Daniels`,
+    }, {
+      picture: `https://api.adorable.io/avatars/128/3`,
+      artist: `Jim Beam`,
+    }],
+  }
+];
+
+
 it(`App should render correctly`, () => {
-  const renderedTree = renderer.create(<App maxErrorsCount={MAX_ERRORS_COUNT} />).toJSON();
+  const renderedTree = renderer.create(<App maxErrorsCount={MAX_ERRORS_COUNT} questions={questions}/>).toJSON();
   expect(renderedTree).toMatchSnapshot();
 });
