fe0923b7a107fb1b763ac40d899fbeb44c372c49
k5va <k5va@yandex.ru>
2020-02-13 09:56:13 +0300

    Added snapshot test for GenreQuestionScreen

---
+++ src/components/genre-question-screen/genre-question-screen.test.js
@@ -0,0 +1,26 @@
+import GenreQuestionScreen from "./genre-question-screen";
+
+const question = {
+  type: `genre`,
+  genre: `rock`,
+  answers: [{
+    src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+    genre: `rock`,
+  }, {
+    src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+    genre: `blues`,
+  }, {
+    src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+    genre: `jazz`,
+  }, {
+    src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+    genre: `rock`,
+  }],
+};
+
+it(`GenreQuestionScreen should render correctly`, () => {
+  const renderedTree = renderer.create(
+      <GenreQuestionScreen question={question} onAnswer={() => {}} />
+  ).toJSON();
+  expect(renderedTree).toMatchSnapshot();
+});
