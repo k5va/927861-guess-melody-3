bc9df534caf5322def277f468596d1cbd36d4cfe
k5va <k5va@yandex.ru>
2020-02-19 12:01:09 +0300

    Added jest moduleNameMapper

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   verbose: true,
   setupFiles: [`./jest.setup.js`],
+  moduleNameMapper: {
+    "@components": `<rootDir>/src/components/`,
+    "@consts": `<rootDir>/src/consts/`,
+    "@mocks": `<rootDir>/src/mocks/`,
+  }
 };
