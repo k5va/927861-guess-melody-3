9c28004bd311c307d2aba035df67336e057591bd
k5va <k5va@yandex.ru>
2020-02-19 16:15:57 +0300

    Added GameScreen

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export {default as App} from "./app/app";
 export {default as WelcomeScreen} from "./welcome-screen/welcome-screen";
 export {default as ArtistQuestionScreen} from "./artist-question-screen/artist-question-screen";
 export {default as GenreQuestionScreen} from "./genre-question-screen/genre-question-screen";
+export {default as GameScreen} from "./game-screen/game-screen";
